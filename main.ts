eeee497b482f4e905130b1f6ea4b20e54d17f228
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-01-31 13:40:10 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,46 @@
+// 1. create two new variables that makes a for loop with a for loop increments up from 1 to 10.
+// The for loop must use the two variables
+let x = 1
+
+let y = 10
+
+for (let index = x; index <= y; index++) {
+    game.splash("counting up: " + index)
+}
+
+
+// 2. create two new variables with a for a loop that counts from 11 to 20
+// The for loop must use the two variables.
+ 
+ let v = 11
+
+ let f = 20
+
+ for (let index = v; index <= f; index++) {
+     game.splash(" counting up to 20:" + index)
+ }
+
+
+
+// 3. create two new variables with a for loop that counts down from 5 to 0
+// The for loop must use the two variables.
+
+let  up = 5
+
+let down = 0
+
+for (let index = up; index >= down; index--) {
+    game.splash("counting down  to 0:" + index)
+}
+
+
+// 4. create two new variables for loop to make a countdown from 100 to 0, skiping 10 numbers each time
+// The for loop must use the two variables.
+ let apple = 100
+
+ let orange = 0
+
+
+ for (let index = apple; index >= orange; =apple) {
+     game.splash("counting down from 100 to 0:" +index)
+ }
